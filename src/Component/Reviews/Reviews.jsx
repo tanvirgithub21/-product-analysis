aa14bf7e5cf82a37f811f5e6fd2f58252bded48c
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-04-05 18:03:53 +0600

    all done

--- src/Component/Reviews/Reviews.jsx
+++ src/Component/Reviews/Reviews.jsx
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Reviews.css";
 import Card from "../Card/Card";
 import useLodeData from "../CustomHooks/useLodeData/useLodeData";
-
-
-
+import Spinner from "../Spinner/Spinner";
 
 const Reviews = () => {
+  const [users, setUsers] = useLodeData();
 
+  const [loding, setLoding] = useState(false);
 
-  const [users, setUsers] = useLodeData()
-  
   return (
     <>
-      <h3 className="text-center mt-7 font-semibold text-2xl">Total Reviews: {users.length}</h3>
-      <div className="reviewsBox">
-        {users.map((user) => (
-          <Card user={user} key={user.id} />
-        ))}
-      </div>
+      {loding ? (
+        <Spinner />
+      ) : (
+        <>
+          <h3 className="text-center mt-7 font-semibold text-2xl">
+            Total Reviews: {users.length}
+          </h3>
+          <div className="reviewsBox">
+            {users.map((user) => (
+              <Card user={user} key={user.id} />
+            ))}
+          </div>
+        </>
+      )}
     </>
   );
 };
